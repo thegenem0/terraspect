636a3f141e0d12ff0a1de5a07d8c2d58e3a4c4df
thegeneme0 <nemethgergo02@gmail.com>
2024-05-06 19:29:07 +0100

    feat: adds web deployment setup

--- infra/infra.go
+++ infra/infra.go
@@ -4,18 +4,23 @@ import (
 	"fmt"
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfront"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfrontorigins"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsecs"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsecspatterns"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awselasticloadbalancingv2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsrds"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsroute53targets"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3assets"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3deployment"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awssecretsmanager"
-	"infra/config"
-
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+	"infra/config"
 )
 
 type InfraStackProps struct {
@@ -47,6 +52,12 @@ func NewInfraStack(scope constructs.Construct, id string, props *InfraStackProps
 			DomainName: jsii.String("*.terraspect.genem0.com"),
 			Validation: awscertificatemanager.CertificateValidation_FromDns(hostedZone),
 		})
+
+	wwwCertificate := awscertificatemanager.Certificate_FromCertificateArn(
+		stack,
+		jsii.String("Terraspect_Cloudfront_Cert"),
+		jsii.String("arn:aws:acm:us-east-1:211125303136:certificate/cae9f667-892e-4212-a526-ab64ca070d99"),
+	)
 
 	// VPC
 
@@ -209,6 +220,113 @@ func NewInfraStack(scope constructs.Construct, id string, props *InfraStackProps
 		Target: awsroute53.RecordTarget_FromAlias(
 			awsroute53targets.NewLoadBalancerTarget(ecsService.LoadBalancer())),
 		RecordName: jsii.String("api.terraspect.genem0.com"), // or use the apex domain
+	})
+
+	webBucketName := "terraspect-web-bucket"
+
+	webBucket := awss3.NewBucket(
+		stack,
+		jsii.String("WebBucket"),
+		&awss3.BucketProps{
+			BucketName: jsii.String(webBucketName),
+			BlockPublicAccess: awss3.NewBlockPublicAccess(
+				&awss3.BlockPublicAccessOptions{
+					BlockPublicAcls:       jsii.Bool(true),
+					BlockPublicPolicy:     jsii.Bool(true),
+					IgnorePublicAcls:      jsii.Bool(true),
+					RestrictPublicBuckets: jsii.Bool(true),
+				},
+			),
+			RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
+		},
+	)
+
+	awss3deployment.NewBucketDeployment(
+		stack,
+		jsii.String("WebDeploymentFiles"),
+		&awss3deployment.BucketDeploymentProps{
+			DestinationBucket: webBucket,
+			Sources: &[]awss3deployment.ISource{
+				awss3deployment.Source_Asset(
+					jsii.String("../terraspect_web/dist"),
+					&awss3assets.AssetOptions{},
+				),
+			},
+		},
+	)
+
+	webCloudfrontDistro := awscloudfront.NewDistribution(
+		stack,
+		jsii.String("WebCloudfrontDistribution"),
+		&awscloudfront.DistributionProps{
+			Enabled: jsii.Bool(true),
+			DefaultBehavior: &awscloudfront.BehaviorOptions{
+				Origin: awscloudfrontorigins.NewS3Origin(
+					webBucket,
+					&awscloudfrontorigins.S3OriginProps{
+						OriginAccessIdentity: awscloudfront.NewOriginAccessIdentity(
+							stack,
+							jsii.String("terraspect-web-OAI"),
+							&awscloudfront.OriginAccessIdentityProps{},
+						),
+					},
+				),
+				ViewerProtocolPolicy: awscloudfront.ViewerProtocolPolicy_REDIRECT_TO_HTTPS,
+			},
+			Certificate:       wwwCertificate,
+			PriceClass:        awscloudfront.PriceClass_PRICE_CLASS_ALL,
+			Comment:           jsii.String("terraspect web distribution"),
+			DefaultRootObject: jsii.String("index.html"),
+			DomainNames: &[]*string{
+				jsii.String("terraspect.genem0.com"),
+				jsii.String("www.terraspect.genem0.com"),
+			},
+			SslSupportMethod: awscloudfront.SSLMethod_SNI,
+			ErrorResponses: &[]*awscloudfront.ErrorResponse{
+				{
+					HttpStatus:         jsii.Number(403),
+					ResponseHttpStatus: jsii.Number(200),
+					ResponsePagePath:   jsii.String("/index.html"),
+				},
+				{
+					HttpStatus:         jsii.Number(404),
+					ResponseHttpStatus: jsii.Number(200),
+					ResponsePagePath:   jsii.String("/index.html"),
+				},
+			},
+		},
+	)
+
+	webBucket.AddToResourcePolicy(
+		awsiam.NewPolicyStatement(
+			&awsiam.PolicyStatementProps{
+				Actions: &[]*string{jsii.String("s3:GetObject")},
+				Effect:  awsiam.Effect_ALLOW,
+				Resources: &[]*string{
+					jsii.Sprintf("arn:aws:s3:::%s", webBucketName),
+					jsii.Sprintf("arn:aws:s3:::%s/*", webBucketName),
+				},
+				Principals: &[]awsiam.IPrincipal{
+					awsiam.NewServicePrincipal(jsii.String("cloudfront.amazonaws.com"), nil),
+				},
+			},
+		),
+	)
+
+	awsroute53.NewARecord(stack, jsii.String("WEB_A_RECORD"), &awsroute53.ARecordProps{
+		Zone: hostedZone,
+		Target: awsroute53.RecordTarget_FromAlias(
+			awsroute53targets.NewCloudFrontTarget(webCloudfrontDistro),
+		),
+		RecordName: jsii.String("terraspect.genem0.com"),
+	})
+
+	awsroute53.NewARecord(stack, jsii.String("WEB_WWW_A_RECORD"), &awsroute53.ARecordProps{
+		Zone: hostedZone,
+		Target: awsroute53.RecordTarget_FromAlias(
+			awsroute53targets.NewCloudFrontTarget(webCloudfrontDistro),
+		),
+		RecordName: jsii.String("www.terraspect.genem0.com"),
 	})
 
 	return stack
